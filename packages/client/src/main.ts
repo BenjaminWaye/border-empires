fd4d24b1d900e87c7096a0e688c7cd4724e8153a
Benjamin Waye <Benjamin Waye>
2026-04-05 22:20:39 +0200

    Restore client stylesheet entry import

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -2,4 +2,5 @@
 // 1. load global client styles
 // 2. hand app assembly to the client app module
 // 3. keep implementation details out of this file
+import "./style.css";
 import "./client-app.js";
